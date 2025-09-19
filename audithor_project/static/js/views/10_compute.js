a1b6d52872ec0c1b0543d5f4895f1462651d63d9
billycuesta <gcuestabasseda@gmail.com>
2025-09-19 12:28:59 +0200

    tag ec2

--- audithor_project/static/js/views/10_compute.js
+++ audithor_project/static/js/views/10_compute.js
@@ -141,6 +141,7 @@ const renderEc2InstancesTable = (instances, allRegions, selectedState = 'all', s
     }
 
     let tableContent = '<div class="overflow-x-auto"><table class="min-w-full divide-y divide-gray-200"><thead class="bg-gray-50"><tr>' +
+                '<th class="px-2 py-3 text-left text-xs font-medium text-gray-500 uppercase">Scope</th>'
                 '<th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase">Region</th>' +
                 '<th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase">Instance ID</th>' +
                 '<th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase">VPC ID</th>' +
@@ -164,7 +165,18 @@ const renderEc2InstancesTable = (instances, allRegions, selectedState = 'all', s
                         </button>`;
         }
 
-        tableContent += `<tr class="hover:bg-gray-50">
+        const scopeDetails = window.scopedResources[i.ARN];
+        const isScoped = !!scopeDetails;
+        const rowClass = isScoped ? 'bg-pink-50 hover:bg-pink-100' : 'hover:bg-gray-50';
+        const scopeComment = isScoped ? scopeDetails.comment : '';
+        const scopeIcon = isScoped 
+            ? `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark-star w-5 h-5 text-pink-600" viewBox="0 0 16 16"><path d="M7.84 4.1a.178.178 0 0 1 .32 0l.634 1.285a.18.18 0 0 0 .134.098l1.42.206c.145.021.204.2.098.303L9.42 6.993a.18.18 0 0 0-.051.158l.242 1.414a.178.178 0 0 1-.258.187l-1.27-.668a.18.18 0 0 0-.165 0l-1.27.668a.178.178 0 0 1-.257-.187l.242-1.414a.18.18 0 0 0-.05-.158l-1.03-1.001a.178.178 0 0 1 .098-.303l1.42-.206a.18.18 0 0 0 .134-.098z"/><path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z"/></svg>` 
+            : `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark-star w-5 h-5 text-gray-400" viewBox="0 0 16 16"><path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z"/></svg>`;
+        const scopeButton = `<button onclick="openScopeModal('${i.ARN}', '${encodeURIComponent(scopeComment)}')" title="${isScoped ? `Marcado: ${scopeComment}` : 'Marcar este recurso'}" class="p-1 rounded-full hover:bg-gray-200 transition">${scopeIcon}</button>`;
+
+
+        tableContent += `<tr class="${rowClass}">
+                    <td class="px-2 py-4 whitespace-nowrap text-sm text-center">${scopeButton}</td>
                     <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-600">${i.Region}</td>
                     <td class="px-4 py-4 whitespace-nowrap text-sm font-medium text-gray-800">${i.InstanceId}</td>
                     <td class="px-4 py-4 whitespace-nowrap text-sm font-mono text-gray-600">${i.VpcId || 'N/A'}</td>
